a9104cabbb6123ce3640752d5d9421e4023c3a9f
seokwon <pam9411@naver.com>
2020-02-07 10:01:48 +0900

    practice JAVA

---
+++ Chapter5/src/classpart/StudentTest.java
@@ -0,0 +1,13 @@
+package classpart;
+
+
+public class StudentTest {
+	public static void main(String [] ar) {
+		
+		Student student = new Student();
+		student.studentName = "호일";
+		student.address = "서울";
+		
+		student.showStudentInfo();
+	}
+}
